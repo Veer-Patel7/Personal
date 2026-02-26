cbd5a3e31c100adc3a55d4e1897a71f1f197f1bb
vansh <vanshpatel3175@gmail.com>
2026-02-26 15:37:35 +0530

    update

--- customer/urls.py
+++ customer/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from .views import *
 
+app_name = "customer"
 urlpatterns = [
     # Customer homepage
     path('', customer_search, name="home"),
-
+    path('search_results/', search_results, name="search_results"), 
+    
     # Hotel and Room details
     path('hotel/<int:hotel_id>/', hotel_detail, name="hotel_detail"),
     path('room/<int:room_id>/', room_select, name="room_select"),
--- customer/views.py
+++ customer/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from hotels.models import Hotel
 from reviews.models import Review
-
+from django.db.models import Min, Max
 @login_required(login_url="/login/")
 def dashboard(request):
     return render(request, "customer/dashboard.html")
 
 def customer_search(request):
-    return render(request, "customer/search.html")
+    hotels = Hotel.objects.all()
+
+    location = request.POST.get("location")
+
+    if location:
+        hotels = hotels.filter(city__icontains=location)
+
+    # Add min and max price from related rooms
+    # hotels = hotels.annotate(
+    #     min_price=Min("rooms__category__price_per_night"),
+    #     max_price=Max("rooms__category__price_per_night")
+    # )
+        
+    return render(request, "customer/search.html", {
+        "hotels": hotels
+    })
+
+def search_results(request):
+    if request.method == "POST":
+        city = request.POST.get("city")
+        check_in = request.POST.get("check_in")
+        check_out = request.POST.get("check_out")
+
+        # For simplicity, we are not filtering based on availability in this example
+        hotels = Hotel.objects.filter(city__icontains=city)
+
+        return render(request, "customer/search_results.html", {"hotels": hotels})
+
+    return redirect("hotel_detail")
 
 def hotel_detail(request, hotel_id):
     return render(request, "customer/hotel_detail.html", {"hotel_id": hotel_id})
