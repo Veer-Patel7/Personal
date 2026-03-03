bcad9fc5874f581f300e08c0cc00cdf2e33acd94
Veer Tkd <veer65patel@gmail.com>
2026-03-03 15:59:49 +0530

    forgot_pass

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,14 @@ urlpatterns = [
     path('signup/', views.customer_signup, name="customer_signup"),
     path("auth/", views.auth_view, name="auth"),
     
+    # path("customer_forgot-password/", views.customer_forgot_password, name="customer_forgot_password"),
+    # path("customer_reset-password/<uidb64>/<token>/", views.customer_reset_password, name="customer_reset_password"),
+    
+    # forgot-password
+    path("forgot-password/", views.forgot_password, name="forgot_password"),
+    path("verify-reset-otp/", views.verify_reset_otp, name="verify_reset_otp"),
+    path("set-new-password/", views.set_new_password, name="set_new_password"),
+    
     # hotel admin
     path('hotel/login/', views.hotel_login, name="hotel_login"),
     path('hotel/signup/', views.hotel_signup, name="hotel_signup"),
--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,18 @@ from django.contrib.auth import get_user_model
 from .utils import generate_otp
 from django.contrib.auth.decorators import login_required
 from hotels.models import Hotel
+# from .forms import SuperAdminLoginForm, UserLoginForm, UserRegistrationForm, ForgotPasswordForm, OTPVerificationForm, SetNewPasswordForm
+from .form import ForgotPasswordForm, OTPVerificationForm, SetNewPasswordForm
+
+# from django.contrib.auth.tokens import default_token_generator
+# from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+# from django.utils.encoding import force_bytes, force_str
+# from django.urls import reverse
+
+
+
+
+
 
 
 User = get_user_model()
@@ -76,7 +88,6 @@ def hotel_login(request):
 
     return render(request, "hotel_login.html")
 
-
 # ========================== SUPER ADMIN LOGIN ==========================
 def super_login(request):
     if request.method == "POST":
@@ -99,9 +110,7 @@ def super_login(request):
 
     return render(request, "super_login.html")
 
-
 # ========================== CUSTOMER SIGNUP WITH OTP ==========================
-
 def customer_signup(request):
 
     if request.method == "POST":
@@ -141,7 +150,6 @@ def customer_signup(request):
         return redirect(f"/verify/?email={email}",user="user")
         
     return render(request, "customer_signup")
-
 
 # ========================== HOTEL ADMIN SIGNUP WITH OTP ==========================
 def hotel_signup(request):
@@ -185,7 +193,6 @@ def hotel_signup(request):
 
     return render(request, "hotel_signup.html")
 
-
 # ========================== VERIFY OTP ==========================
 def verify(request):
 
@@ -217,7 +224,6 @@ def verify(request):
 
     return render(request, "customer/verify.html", {"email": email})
 
-
 # ========================== LOGOUT ==========================
 def user_logout(request):
     logout(request)
@@ -225,3 +231,163 @@ def user_logout(request):
 
 def auth_view(request):
     return render(request, "customer/auth.html")
+
+# ========================== FORGOT PASSWORD ==========================
+def forgot_password(request):
+    if request.method == "POST":
+        form = ForgotPasswordForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+
+            try:
+                user = User.objects.get(email=email)
+
+                otp = generate_otp()
+                user.otp = otp
+                user.save()
+
+                # ✅ VERY IMPORTANT
+                request.session["reset_email"] = email
+
+                send_mail(
+                    "Password Reset OTP",
+                    f"Your OTP is {otp}",
+                    settings.EMAIL_HOST_USER,
+                    [email],
+                )
+
+                return redirect("accounts:verify_reset_otp")
+
+            except User.DoesNotExist:
+                form.add_error("email", "Email not registered")
+    else:
+        form = ForgotPasswordForm()
+
+    return render(request, "accounts/forgotpassword.html", {"form": form})
+
+# ========================== VERIFY RESET OTP ==========================
+def verify_reset_otp(request):
+    email = request.session.get("reset_email")
+
+    if not email:
+        return redirect("accounts:forgot_password")
+
+    if request.method == "POST":
+        form = OTPVerificationForm(request.POST)
+
+        if form.is_valid():
+            entered_otp = form.cleaned_data["otp"]
+
+            user = User.objects.get(email=email)
+
+            if user.otp == entered_otp:
+                return redirect("accounts:set_new_password")
+            else:
+                form.add_error("otp", "Invalid OTP")
+    else:
+        form = OTPVerificationForm()
+
+    return render(request, "accounts/verify_reset_otp.html", {"form": form})
+
+# ========================== SET NEW PASSWORD ==========================
+def set_new_password(request):
+    email = request.session.get("reset_email")
+
+    if not email:
+        return redirect("accounts:forgot_password")
+
+    if request.method == "POST":
+        form = SetNewPasswordForm(request.POST)
+
+        if form.is_valid():
+            password = form.cleaned_data["password"]
+
+            user = User.objects.get(email=email)
+
+            user.set_password(password)
+            user.otp = ""
+            user.save()
+
+            # clear session
+            del request.session["reset_email"]
+
+            if user.role == "customer":
+                return redirect("accounts:customer_login")
+            elif user.role == "hotel_admin":
+                return redirect("accounts:hotel_login")
+            elif user.role == "super_admin":
+                return redirect("accounts:super_login")
+    else:
+        form = SetNewPasswordForm()
+
+    return render(request, "accounts/set_new_password.html", {"form": form})
+
+# # ========================== FORGOT PASSWORD ==========================
+# def customer_forgot_password(request):
+#     if request.method == "POST":
+#         email = request.POST.get("email")
+
+#         try:
+#             user = User.objects.get(email=email)
+
+#             # ✅ ROLE CHECK
+#             if user.role == "customer" and user.is_verified:
+
+#                 uid = urlsafe_base64_encode(force_bytes(user.pk))
+#                 token = default_token_generator.make_token(user)
+
+#                 reset_link = request.build_absolute_uri(
+#                     reverse("accounts:customer_reset_password", args=[uid, token])
+#                 )
+
+#                 send_mail(
+#                     "Reset Your Password",
+#                     f"Click the link to reset password:\n{reset_link}",
+#                     settings.EMAIL_HOST_USER,
+#                     [email],
+#                     fail_silently=False,
+#                 )
+            
+#         except User.DoesNotExist:
+#             pass
+
+#         messages.success(request, "Password reset link sent to your email")
+#         return redirect("accounts:auth")
+
+
+
+#     return render(request, "customer/forgot_password.html")
+
+# # ========================== RESET PASSWORD ==========================
+# def customer_reset_password(request, uidb64, token):
+#     try:
+#         uid = force_str(urlsafe_base64_decode(uidb64))
+#         user = User.objects.get(pk=uid)
+#     except:
+#         user = None
+
+#     if user and default_token_generator.check_token(user, token):
+
+#         if user.role != "customer":
+#             messages.error(request, "Unauthorized access.")
+#             return redirect("accounts:auth")
+
+#         if request.method == "POST":
+#             password = request.POST.get("password")
+#             confirm = request.POST.get("confirm_password")
+
+#             if password != confirm:
+#                 messages.error(request, "Passwords do not match")
+#                 return redirect(request.path)
+
+#             user.set_password(password)
+#             user.save()
+
+#             messages.success(request, "Password reset successful")
+#             return redirect("accounts:auth")
+
+#         return render(request, "customer/reset_password.html")
+
+#     messages.error(request, "Invalid or expired link")
+#     return redirect("accounts:auth")
